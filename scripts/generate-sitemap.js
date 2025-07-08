ff11df9d88e2f19045f57bbc819ef433d7571bb9
Amritanshu Mishra <amritanshu99@gmail.com>
2025-07-08 22:49:22 +0530

    Adding movie recommender

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -13,7 +13,8 @@ async function generateSitemap() {
     { url: "/tech-byte", changefreq: "daily", priority: 1.0 },
     { url: "/ai-tools", changefreq: "monthly", priority: 0.5 },
     { url: "/task-manager", changefreq: "weekly", priority: 0.8 },
-     { url: "/spam-check", changefreq: "weekly", priority: 0.8 },
+    { url: "/spam-check", changefreq: "weekly", priority: 0.8 },
+    { url: "/movie-recommender", changefreq: "weekly", priority: 0.8 },
   ];
 
   const sitemapStream = new SitemapStream({ hostname });
