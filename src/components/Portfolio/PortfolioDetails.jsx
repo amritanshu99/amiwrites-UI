a1ddb26663c9f22e86133cdac102d30e97abb83e
Amritanshu Mishra <amritanshu99@gmail.com>
2025-09-25 16:26:59 +0530

    Cosmetic changes

--- src/components/Portfolio/PortfolioDetails.jsx
+++ src/components/Portfolio/PortfolioDetails.jsx
@@ -348,7 +348,11 @@ export default function Portfolio() {
         <article className="bg-white dark:bg-zinc-900 text-black dark:text-white bg-opacity-90 backdrop-blur-md rounded-xl shadow-xl max-w-4xl w-full p-6 md:p-10">
           <section className="flex flex-col sm:flex-row items-center gap-6 sm:gap-10">
             <motion.img
-              src={`https://amiwrites-backend-app-2lp5.onrender.com${data.photoUrl}`}
+          src={
+    document.documentElement.classList.contains("dark")
+      ? `https://amiwrites-backend-app-2lp5.onrender.com${data.photoUrlDark}`
+      : `https://amiwrites-backend-app-2lp5.onrender.com${data.photoUrl}`
+  }
               alt="Amritanshu Mishra"
               loading="eager"
               width={192}
